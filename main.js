cd96bf60e634b5f5912c9c1b26e0bd5ec95907ea
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-06-02 00:29:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var song="";
 
+scoreRightWrist=0;
+scoreLeftWrist=0;
+
+rightWristX=0;
+rightWristY=0;
+
+leftWristY=0;
+leftWristX=0;
+
 function setup()
 {
     canvas=createCanvas(600,500);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
@@ -26,6 +37,34 @@ function play()
 function stop()
 {
     song.stop();
+}
+
+function modalLoaded()
+{
+console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+if (results.length>0)
+{
+    console.log(results);
+
+    scoreRightWrist=results[0].pose.keypoints[10].score;
+    scoreleftWrist=results[0].pose.keypoints[9].score;
+
+    console.log("scoreLeftWrist="+scoreLeftWrist+"scoreRightWrist="+scoreRightWrist);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+
+    console.log("leftWristX="+leftWristX+", leftWristY="+leftWristY);
+
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+
+    console.log("rightWristX="+rightWristX+", rightWristY="+rightWristY);
+}
 }
 
 /*var peter_pan = "";
